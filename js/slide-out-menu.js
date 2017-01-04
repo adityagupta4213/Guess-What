cb6620b4bc262a121696346cb75377844f1b43f3
Aditya J Gupta <adityagupta4213@yandex.com>
2017-01-04 17:22:51 +0530

    Added hamburger menu

---
+++ js/slide-out-menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $('.button-collapse').sideNav({
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      width:240,
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+ });
